da3d3c16c9185268938a60aa31681890f6a00ce3
cheonyulin@gmail.com <cheonyulin@gmail.com>
2022-05-09 01:21:49 +0900

    add create todo function logic

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,53 @@
 <script>
+	import Header from './components/layouts/Header.svelte';
+	import Main from './components/layouts/Main.svelte';
+	import Footer from './components/layouts/Footer.svelte';
+
+	let todos = [
+        {
+            id: 1,
+            done: false,
+            text: '스벨트 공부하기'
+        },
+        {
+            id: 2,
+            done: false,
+            text: '운동하기'
+        },
+        {
+            id: 3,
+            done: true,
+            text: 'Angular Universal 공부하기'
+        }
+    ]
+
+	function onCreateTodo(event) {
+		const detail = event.detail;
+		const newId = todos.length + 1;
+		const newTodo = {
+			id: newId,
+			done: false,
+			text: detail.text
+		}
+		todos = [...todos, newTodo];
+	}
 </script>
 
-<main>
-</main>
+<section class="container">
+	<Header/>
+	<Main {todos}/>
+	<Footer on:create={onCreateTodo}/>
+</section>
 
 <style>
-	html {
-		font-size: 10px;
+	.container {
+		position: absolute;
+		top: 40%;
+		left: 50%;
+		transform: translate(-50%, -40%);
+		width: 100%;
+		max-width: 42rem;
+		border: 0.1rem solid #ddd;
+		padding: 2.4rem;
 	}
 </style>
---
+++ src/components/layouts/Footer.svelte
@@ -0,0 +1,76 @@
+<script>
+    import {createEventDispatcher} from 'svelte';
+
+    const dispatch = createEventDispatcher();
+
+    let content;
+    let target;
+
+    function handleSubmit() {
+        dispatch('create', {
+            text: content
+        })
+        target.value = '';
+    }
+</script>
+
+<footer>
+    <form on:submit|preventDefault={handleSubmit}>
+        <section class="input-section">
+            <input 
+                type="text" 
+                bind:value={content}
+                bind:this={target}
+            >
+        </section>
+        <section class="submit-section">
+            <button>추가</button>
+        </section>
+    </form>
+</footer>
+
+<style>
+    footer {
+        margin-top: 2.4rem;
+    }
+
+    footer form {
+        display: grid;
+        grid-template-columns: 0.7fr 0.3fr;
+    }
+
+    footer form section {
+        height: 3.6rem;
+    }
+
+    footer form section.submit-section {
+        margin-left: 1.2rem;
+    }
+
+    footer form input {
+        width: 100%;
+        height: 100%;
+        border: 0.1rem solid #ddd;
+        font-size: 1.4rem;
+        line-height: 1.9rem;
+        padding: 0.8rem;
+    }
+
+    footer form input:focus {
+        outline: none;
+        border: 0.1rem solid #A7AFFE;
+    }
+
+    footer form button {
+        width: 100%;
+        height: 100%;
+        cursor: pointer;
+        background: #A7AFFE;
+        color: #fff;
+        font-size: 1.4rem;
+        line-height: 1.9rem;
+        border: 0.1rem solid #ddd;
+        margin: 0;
+        padding: 0;
+    }
+</style>
